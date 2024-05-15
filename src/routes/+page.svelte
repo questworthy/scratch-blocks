d6ec48a796a6045211ccab778f82c7ba31707ebb
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-15 11:26:32 +0530

    adds basic nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import logo from '$lib/assets/logo.svg';
+</script>
+
+<div class="mt-0 h-12 bg-[#855CD6] flex justify-between items-center">
+	<a href="https://scratch.mit.edu/">
+		<img src={logo} alt="" class="m-0 mx-4 h-[1.6rem]" />
+	</a>
+	<a
+		class="m-0 px-4 no-underline text-xs font-bold text-white h-full hover:bg-[#714EB6] content-center"
+		href="https://scratch.mit.edu/"
+	>
+		Visit Scratch
+	</a>
+</div>
