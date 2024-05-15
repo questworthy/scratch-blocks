4fa5be612b5a5bc2d33631d5c09111e45d22a0e0
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-15 15:08:27 +0530

    creates blocks in svg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,12 @@
 	let isOpen = false;
 </script>
 
-<!-- Navigation Bar --------------------------------------------------------------------->
+<!-- Navigation Bar ---------------------------------------------------------->
 
-<div class="mt-0 h-12 bg-[#855CD6] flex justify-between items-center text-xs">
+<div class="mt-0 h-16 bg-[#855CD6] flex justify-between items-center">
 	<div class="flex justify-center items-center">
 		<a href="https://scratch.mit.edu/">
-			<img src={logo} alt="" class="m-0 mx-4 h-[1.6rem]" />
+			<img src={logo} alt="" class="m-0 mx-4 h-8" />
 		</a>
 
 		<a class={linkStyle} target="_blank" href={rickroll}> Home </a>
@@ -70,43 +70,57 @@
 	</div>
 {/if}
 
-<!-- Navigation Bar --------------------------------------------------------------------->
+<!-- Landing content --------------------------------------------------------->
 
-<svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-	<path
-		class="blocklyPath blocklyBlockBackground"
-		stroke="#774DCB"
-		fill="#9966FF"
-		fill-opacity="1"
-		d="m 0,4 A 4,4 0 0,1 4,0 H 12 c 2,0 3,1 4,2 l 4,4 c 1,1 2,2 4,2 h 12 c 2,0 3,-1 4,-2 l 4,-4 c 1,-1 2,-2 4,-2 H 114.83950805664062 a 4,4 0 0,1 4,4 v 40  a 4,4 0 0,1 -4,4 H 48   c -2,0 -3,1 -4,2 l -4,4 c -1,1 -2,2 -4,2 h -12 c -2,0 -3,-1 -4,-2 l -4,-4 c -1,-1 -2,-2 -4,-2 H 4 a 4,4 0 0,1 -4,-4 z"
-	></path><g
-		data-id="wR?-0@_Y9#hF:k)#J.(h"
-		data-argument-type="text"
-		data-shapes="argument round"
-		transform="translate(48,8.000000000000114)"
-		><path
-			class="blocklyPath blocklyBlockBackground"
-			stroke="#774DCB"
-			fill="#FFFFFF"
-			fill-opacity="1"
-			d="m 0,0 m 16,0 H 46.839508056640625 a 16 16 0 0 1 0 32 H 16 a 16 16 0 0 1 0 -32 z"
-		></path><g class="blocklyEditableText" style="cursor: text;" transform="translate(8, 0) "
-			><text
-				class="blocklyText"
-				x="23.419754028320312"
-				y="18"
-				dominant-baseline="middle"
-				dy="0"
-				text-anchor="middle">Hello!</text
-			></g
-		></g
-	><text
-		class="blocklyText"
-		y="2"
-		text-anchor="middle"
-		dominant-baseline="middle"
-		dy="0"
-		x="12.222222328186035"
-		transform="translate(8, 24) ">say</text
-	><path class="blocklyPath" style="visibility: hidden" d="" fill="#774DCB"></path></svg
->
+<div class="w-80 h-40">
+	<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" class="bg-red-200">
+		<g transform="translate(10, 25)">
+			<path
+				class=""
+				stroke="#CC9900"
+				fill="#FFBF00"
+				fill-opacity="1"
+				d="m 0,0 c 25,-22 71,-22 96,0 H 260  a 4,4 0 0,1 4,4 v 40  a 4,4 0 0,1 -4,4 H 48   c -2,0 -3,1 -4,2 l -4,4 c -1,1 -2,2 -4,2 h -12 c -2,0 -3,-1 -4,-2 l -4,-4 c -1,-1 -2,-2 -4,-2 H 4 a 4,4 0 0,1 -4,-4 z"
+			>
+			</path>
+			<text fill="white" stroke="white" stroke-width="0.5" transform="translate(10,30)">
+				when this sprite clicked
+			</text>
+		</g>
+		<g transform="translate(10, 74)">
+			<path
+				class="blocklyPath blocklyBlockBackground"
+				stroke="#BD42BD"
+				fill="#CF63CF"
+				fill-opacity="1"
+				d="m 0,4 A 4,4 0 0,1 4,0 H 12 c 2,0 3,1 4,2 l 4,4 c 1,1 2,2 4,2 h 12 c 2,0 3,-1 4,-2 l 4,-4 c 1,-1 2,-2 4,-2 H 220 a 4,4 0 0,1 4,4 v 40  a 4,4 0 0,1 -4,4 H 48   c -2,0 -3,1 -4,2 l -4,4 c -1,1 -2,2 -4,2 h -12 c -2,0 -3,-1 -4,-2 l -4,-4 c -1,-1 -2,-2 -4,-2 H 4 a 4,4 0 0,1 -4,-4 z"
+			>
+			</path>
+			<g transform="translate(130,8)">
+				<path
+					transition:draw={{ duration: 5000, delay: 500, easing: quintOut }}
+					stroke="#BD42BD"
+					fill="#C94FC9"
+					fill-opacity="1"
+					d="m 0,0 m 16,0 H 60 a 16 16 0 0 1 0 32 H 16 a 16 16 0 0 1 0 -32 z"
+				></path>
+				<g class="blocklyEditableText" style="cursor: default;" transform="translate(8, 0) "
+					><text
+						x="25"
+						y="18"
+						fill="white"
+						stroke="white"
+						stroke-width="0.5"
+						dominant-baseline="middle"
+						dy="0"
+						text-anchor="middle">Meow</text
+					>
+				</g>
+			</g>
+
+			<text fill="white" stroke="white" stroke-width="0.5" transform="translate(10,30)">
+				start sound
+			</text>
+		</g>
+	</svg>
+</div>
